dc0a71ecfc9cf68b45c13c4e985865785c261f83
bede <junohsheji@gmail.com>
2022-11-16 17:59:04 +0900

    env

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool, cpu_count
 
 
 def agricultural_hours(db_path):
-    occupancy_file = f"{db_path}/archetypes/use_types/AGRICULTURAL.csv"
+    occupancy_file = f"{db_path}/inputs/technology/archetypes/use_types/AGRICULTURAL.csv"
     occupancy = pd.read_csv(occupancy_file, header=2)
     result = {}
     for day in ['WEEKDAY', 'SATURDAY', 'SUNDAY']:
@@ -15,56 +15,38 @@ def agricultural_hours(db_path):
     return total_hours, result
 
 
-def fill_cooking(row, usage, area, path, cooking_loads):
-    total_hours, result = cooking_hours(path, usage)
+def fill_agriculture(row, usage, area, path, energy_per_sqm):
+    total_hours, result = agricultural_hours(path)
     week_day = result['WEEKDAY']
     saturday = result['SATURDAY']
     sunday = result['SUNDAY']
     day = datetime.fromisoformat(row['DATE']).weekday()
     hour = datetime.fromisoformat(row['DATE']).time().hour
-    if day in range(5):
-        if hour in week_day:
-            return cooking_loads[usage] * area / total_hours
-        else:
-            return 0
-    elif day == 5:
-        if hour in saturday:
-            return cooking_loads[usage] * area / total_hours
-        else:
-            return 0
-    elif day == 6:
-        if hour in sunday:
-            return cooking_loads[usage] * area / total_hours
-        else:
-            return 0
-    else:
-        return 0
-
-
-def fill_agriculture(row, usage, area):
-    total_hours, week_day, sat_day, sun_day = agricultural_hours()
-    day = datetime.fromisoformat(row['DATE']).weekday()
-    hour = datetime.fromisoformat(row['DATE']).time().hour
     if usage == 'AGRICULTURAL':
         if day in range(5):
-            if week_day[hour] != 0:
-                return 5.543257971 * area * 0.2777778 * 37.7 / total_hours
-            else:
-                return 0
+            return energy_per_sqm * area / total_hours * week_day[hour]
+
         elif day == 5:
-            if sat_day[hour] != 0:
-                return 5.543257971 * area * 0.2777778 * 37.7 / total_hours
-            else:
-                return 0
+            return energy_per_sqm * area / total_hours * saturday[hour]
+
         elif day == 6:
-            if sun_day[hour] != 0:
-                return 5.543257971 * area * 0.2777778 * 37.7 / total_hours
-            else:
-                return 0
+            return energy_per_sqm * area / total_hours * sunday[hour]
         else:
             return 0
     else:
         return 0
+
+
+def calculate_agriculture_loads(item):
+    path = item['path']
+    area = item['AREA']
+    usage = item['1ST_USE']
+    energy_df = pd.read_csv('agriculture.csv')
+    energy_df['result'] = energy_df['energy_per_animal'] * energy_df['animal_num'] / energy_df['floor_area_total']
+    energy = sum(energy_df['result'])
+    data = pd.read_csv(path)
+    data['agricultural'] = data.apply(fill_agriculture, axis=1, args=(usage, area, path, energy))
+    data.to_csv(path)
 
 
 def get_building_info(db_path):
@@ -80,35 +62,17 @@ def get_building_info(db_path):
     return building
 
 
-def get_cooking_loads(db_path, energy_type):
-    building_info = get_building_info(db_path)
-    internal_loads = pd.read_excel(f"{db_path}/inputs/technology/archetypes/use_types/USE_TYPE_PROPERTIES.xlsx",
-                                   sheet_name='INTERNAL_LOADS')
-    internal_loads.set_index('code', inplace=True)
-    internal_loads_dict = internal_loads.to_dict()
-    cooking_loads = internal_loads_dict[f'{energy_type}cook_kWhm2']
-    building_info['cooking'] = building_info.apply(lambda x: cooking_loads[x['1ST_USE']], axis=1)
-    building_info = building_info[['Name', 'AREA', 'cooking', '1ST_USE']]
-    return building_info
-
-
-def calculate_cooking_loads(item):
-    path = item['path']
-    area = item['AREA']
-    cook = item['cooking']
-    usage = item['1ST_USE']
-    data = pd.read_csv(path)
-    data['cooking'] = data.apply(fill_cooking, axis=1, args=(usage, area, path, cook))
-    data.to_csv(path)
-
-
-def process_cooking_loads(db_path, energy_type):
+def process_agriculture_loads(db_path, energy_type, multi_processing=True):
     check_energy_type(energy_type)
-    building_info = get_cooking_loads(db_path, energy_type)
+    building_info = get_building_info(db_path)
     building_info['path'] = db_path
     data = list(building_info.transpose().to_dict())
-    multi = cpu_count() - 1
-    Pool(multi).map(calculate_cooking_loads, data)
+    if multi_processing:
+        multi = cpu_count() - 1
+        Pool(multi).map(calculate_agriculture_loads, data)
+    else:
+        for datum in data:
+            calculate_agriculture_loads(datum)
 
 
 def check_energy_type(energy_type):
@@ -123,9 +87,9 @@ def main():
     db_path = input("Please input CEA scenario path: ")
     multi = input("Are you going to run multiprocessing? (y/n) : ")
     if multi == 'y':
-        process_cooking_loads(db_path, energy_type)
+        process_agriculture_loads(db_path, energy_type)
     elif multi == 'n':
-        process_cooking_loads(db_path, energy_type, multi_processing=False)
+        process_agriculture_loads(db_path, energy_type, multi_processing=False)
 
 
 if __name__ == '__main__':
